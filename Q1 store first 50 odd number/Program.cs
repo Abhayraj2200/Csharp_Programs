dc35f15664fb06cc621aa60486784acafaa489d7
Abhayraj2200 <78071233+Abhayraj2200@users.noreply.github.com>
2021-03-11 18:22:33 +0530

    Q1 Completed

---
+++ Q1 store first 50 odd number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Q1_store_first_50_odd_number
+{
+    //Write a program that stores first 50 odd numbers to an array
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOdd = new int[25]; //Declaring an array of size 25
+            for (int i = 1; i <= 25; i++) //To count from 1 to 25
+            {
+                arrayOdd[i - 1] = 2 * i - 1; //Using to store odd values to index
+            }
+            for (int i = 0; i < 25; i++) //To print the array from index 0 to 25
+            {
+                Console.WriteLine(arrayOdd[i]); // Print Each Index value
+            }
+        }
+    }
+}
